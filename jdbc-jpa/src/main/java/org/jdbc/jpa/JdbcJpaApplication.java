13c27f88cb3a0286a995a1b02be5e772d33a5ed0
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2019-01-24 09:23:44 +0530

    Adding Project related to jpa

---
+++ jdbc-jpa/src/main/java/org/jdbc/jpa/JdbcJpaApplication.java
@@ -0,0 +1,14 @@
+package org.jdbc.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcJpaApplication.class, args);
+	}
+
+}
+
